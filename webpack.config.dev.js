efa5a417e2d099666da5cd016340cfa33273c244
estebansant <estebansant30@gmail.com>
2022-03-26 16:28:15 -0300

    setting up everything to start working on the project

---
+++ webpack.config.dev.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: "/",
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html',
+        }),
+        new MiniCSSExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000,
+    }
+}
