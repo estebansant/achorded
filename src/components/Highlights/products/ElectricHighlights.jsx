79fd10b1ab0949954cd6d166505130d97937c1e0
estebansant <estebansant30@gmail.com>
2022-07-16 12:36:41 -0300

    renamed the guitars

--- src/components/Highlights/products/ElectricHighlights.jsx
+++ src/components/Highlights/products/ElectricHighlights.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './ProductsHighlights.scss';
-import triangleRed from '@images/electric/electric_triangle_red-1.svg';
-import classicWood from '@images/electric/electric_wood.svg';
-import rockBlack from '@images/electric/electric_black-1.svg';
+import stratocaster from '@images/electric/stratocaster-red.svg';
+import telecaster from '@images/electric/telecaster-brown.svg';
+import jacksonSoloist from '@images/electric/jackson_soloist-black.svg';
 
 const ElectricHighlights = () => {
   return (
@@ -10,30 +10,30 @@ const ElectricHighlights = () => {
         <section className="wrapper">
             <div className="showcase">
                 <a className="showcase-link" href="/electric-guitars/smokey">
-                    <img className="showcase__image" src={triangleRed} alt="red triangle electric guitar"/>
+                    <img className="showcase__image" src={stratocaster} alt="stratocaster red electric guitar"/>
                     <div className="showcase__text--container">
-                        <h4>The Smokey</h4>
-                        <p>The trustworthy triangle.</p>
+                        <h4>The Stratocaster</h4>
+                        <p>A trustworthy Fender guitar.</p>
                     </div>
                 </a>
             </div>
             
             <div className="showcase">
                 <a className="showcase-link" href="/electric-guitars/old-fashioned">
-                    <img className="showcase__image" src={classicWood} alt="wooden electric guitar"/>
+                    <img className="showcase__image" src={telecaster} alt="telecaster wooden guitar"/>
                     <div className="showcase__text--container">
-                        <h4>Old Fashioned Way</h4>
-                        <p>A classic pleasure.</p>
+                        <h4>Fender Telecaster</h4>
+                        <p>Our classical pleasure.</p>
                     </div>
                 </a>
             </div>
             
             <div className="showcase">
                 <a className="showcase-link" href="/electric-guitars/classical-rock">
-                    <img className="showcase__image" src={rockBlack} alt="black electric guitar"/>
+                    <img className="showcase__image" src={jacksonSoloist} alt="jackson soloist black electric guitar"/>
                     <div className="showcase__text--container">
-                        <h4>Classical Rock</h4>
-                        <p>The favorite of many.</p>
+                        <h4>Jackson Soloist</h4>
+                        <p>Black in color, pure of soul.</p>
                     </div>
                 </a>
             </div>
--- src/components/Story/Story.jsx
+++ src/components/Story/Story.jsx
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import triangleRed from '@images/electric/electric_triangle_red-1.svg';
+import vModelGibson from '@images/electric/v_model_gibson-red.svg';
 
 const Story = () => {
     return(
@@ -34,135 +34,15 @@ const Story = () => {
                 pagination={{
                     el: '.swiper-pagination',
                     clickable: true,}}
-                loop={true}
+                loop={false}
                 className="story__swiper"
                 >
                 <SwiperSlide className="story__swiper--slide">
                     <a className="swiper__slide--link">
                         <h3 className="swiper__slide--title">Triangle Red Guitar</h3>
-                        <img className="swiper__slide--image"src={triangleRed} alt="dabois"/>
+                        <img className="swiper__slide--image"src={vModelGibson} alt="stratocaster red guitar"/>
                         <div className="swiper__slide--writing">
                             <p className="swiper__slide--writing__text">This is a small paragraph about the story of this particular guitar, and how it came to be what it is today.</p>
-                        </div>
-                        
-                    </a>
-                </SwiperSlide>
-                <SwiperSlide className="story__swiper--slide">
-                    <a className="swiper__slide--link">
-                        <h3 className="swiper__slide--title">Triangle Red Guitar</h3>
-                        <img className="swiper__slide--image"src={triangleRed} alt="dabois"/>
-                        <div className="swiper__slide--writing">
-                            <p className="swiper__slide--writing__text">This is a smragraph about the story of this particular guitar, and how it came to be what it is today.</p>
-                        </div>
-                        
-                    </a>
-                </SwiperSlide>
-                <SwiperSlide className="story__swiper--slide">
-                    <a className="swiper__slide--link">
-                        <h3 className="swiper__slide--title">Triangle uitar</h3>
-                        <img className="swiper__slide--image"src={triangleRed} alt="dabois"/>
-                        <div className="swiper__slide--writing">
-                            <p className="swiper__slide--writing__text">This is a small paragraph about the story of this particular guitar, and how it came to be what it is today.</p>
-                        </div>
-                        
-                    </a>
-                </SwiperSlide>
-                <SwiperSlide className="story__swiper--slide">
-                    <a className="swiper__slide--link">
-                        <h3 className="swiper__slide--title">Triangle Red Guitar</h3>
-                        <img className="swiper__slide--image"src={triangleRed} alt="dabois"/>
-                        <div className="swiper__slide--writing">
-                            <p className="swiper__slide--writing__text">This is a small paragraph about the story of this particular guitar, and how it came to be what it is today.</p>
-                        </div>
-                        
-                    </a>
-                </SwiperSlide>
-                <SwiperSlide className="story__swiper--slide">
-                    <a className="swiper__slide--link">
-                        <h3 className="swiper__slide--title">Triangle Red Guitar</h3>
-                        <img className="swiper__slide--image"src={triangleRed} alt="dabois"/>
-                        <div className="swiper__slide--writing">
-                            <p className="swiper__slide--writing__text">This is a saragraph about tory of this particular guitar, and how it came to be what it is today.</p>
-                        </div>
-                        
-                    </a>
-                </SwiperSlide>
-                <SwiperSlide className="story__swiper--slide">
-                    <a className="swiper__slide--link">
-                        <h3 className="swiper__slide--title">Triangle Red Gtar</h3>
-                        <img className="swiper__slide--image"src={triangleRed} alt="dabois"/>
-                        <div className="swiper__slide--writing">
-                            <p className="swiper__slide--writing__text">This is a small paragraph about the story of this particular guitar, and how it came to be what it is today.</p>
-                        </div>
-                        
-                    </a>
-                </SwiperSlide>
-                <SwiperSlide className="story__swiper--slide">
-                    <a className="swiper__slide--link">
-                        <h3 className="swiper__slide--title">Triangle Red Guitar</h3>
-                        <img className="swiper__slide--image"src={triangleRed} alt="dabois"/>
-                        <div className="swiper__slide--writing">
-                            <p className="swiper__slide--writing__text">This is a small paragraph about the sof this particular guitar, and how it came to be what it is today.</p>
-                        </div>
-                        
-                    </a>
-                </SwiperSlide>
-                <SwiperSlide className="story__swiper--slide">
-                    <a className="swiper__slide--link">
-                        <h3 className="swiper__slide--title">Trangle Red Guitar</h3>
-                        <img className="swiper__slide--image"src={triangleRed} alt="dabois"/>
-                        <div className="swiper__slide--writing">
-                            <p className="swiper__slide--writing__text">This is a small paragraph about the story of this particular guitar, and how it came to be what it is today.</p>
-                        </div>
-                        
-                    </a>
-                </SwiperSlide>
-                <SwiperSlide className="story__swiper--slide">
-                    <a className="swiper__slide--link">
-                        <h3 className="swiper__slide--title">Triangle Red Guitar</h3>
-                        <img className="swiper__slide--image"src={triangleRed} alt="dabois"/>
-                        <div className="swiper__slide--writing">
-                            <p className="swiper__slide--writing__text">This is a small paragraph of this picular guitar, and how it came to be what it is today.</p>
-                        </div>
-                        
-                    </a>
-                </SwiperSlide>
-                <SwiperSlide className="story__swiper--slide">
-                    <a className="swiper__slide--link">
-                        <h3 className="swiper__slide--title">Tringle Red Guitar</h3>
-                        <img className="swiper__slide--image"src={triangleRed} alt="dabois"/>
-                        <div className="swiper__slide--writing">
-                            <p className="swiper__slide--writing__text">This is a paragraph about the story of this particular guitar, and how it came to be what it is today.</p>
-                        </div>
-                        
-                    </a>
-                </SwiperSlide>
-                <SwiperSlide className="story__swiper--slide">
-                    <a className="swiper__slide--link">
-                        <h3 className="swiper__slide--title">Triangle Guitar</h3>
-                        <img className="swiper__slide--image"src={triangleRed} alt="dabois"/>
-                        <div className="swiper__slide--writing">
-                            <p className="swiper__slide--writing__text">This s a small paragraph about the story of this particular guitar, and how it came to be what it is today.</p>
-                        </div>
-                        
-                    </a>
-                </SwiperSlide>
-                <SwiperSlide className="story__swiper--slide">
-                    <a className="swiper__slide--link">
-                        <h3 className="swiper__slide--title">Triangle Red Guitar</h3>
-                        <img className="swiper__slide--image"src={triangleRed} alt="dabois"/>
-                        <div className="swiper__slide--writing">
-                            <p className="swiper__slide--writing__text">T is a small paragraph about the story of this particular guitar, and how it came to be what it is today.</p>
-                        </div>
-                        
-                    </a>
-                </SwiperSlide>
-                <SwiperSlide className="story__swiper--slide">
-                    <a className="swiper__slide--link">
-                        <h3 className="swiper__slide--title">Triangle Rd Guitar</h3>
-                        <img className="swiper__slide--image"src={triangleRed} alt="dabois"/>
-                        <div className="swiper__slide--writing">
-                            <p className="swiper__slide--writing__text">This is a small parag about the story of this particular guitar, and how it came to be what it is today.</p>
                         </div>
                         
                     </a>
