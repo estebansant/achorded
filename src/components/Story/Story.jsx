ee4f07cba30a7dd7a86b9471656eaaf5a17511a5
estebansant <estebansant30@gmail.com>
2022-07-24 14:32:01 -0300

    finally finished the carousel!

--- src/components/Story/Story.jsx
+++ src/components/Story/Story.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import './Story.scss';
 import { Carousel } from './carousel/Carousel';
-import { CarouselData } from './carousel/CarouselData';
-// import { Navigation, Pagination } from 'swiper';
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import 'swiper/scss';
-// import 'swiper/scss/navigation';
-// import 'swiper/scss/pagination';
-// import vModelGibson from '@images/electric/v_model_gibson-red.svg';
-// import stratocaster from '@images/electric/stratocaster-red.svg';
-// import telecaster from '@images/electric/telecaster-brown.svg';
-// import jacksonSoloist from '@images/electric/jackson_soloist-black.svg';
 
 const Story = () => {
     return(
         <section className="story">
             <h2 className="story__title">Know their story.</h2>
-            <Carousel
-                slides={CarouselData}
-            />
+            <Carousel />
         </section>
     )
 }
--- src/components/Story/carousel/Carousel.jsx
+++ src/components/Story/carousel/Carousel.jsx
@@ -2,34 +2,43 @@ import React from 'react';
 import './Carousel.scss';
 import Slider from "react-slick";
 import { CarouselData } from './CarouselData';
-// import rightArrow from '@icons/arrow-right.svg';
-// import leftArrow from '@icons/arrow-left.svg';
+import rightArrow from '@icons/arrow-right.svg';
+import leftArrow from '@icons/arrow-left.svg';
 
-const Carousel = ({slides}) => {
-    // const [section, setSection] = React.useState(0);
-    // const length = slides.length;
+const Carousel = () => {
 
-    // const nextSlide = () => {
-    //     setSection(section === length - 1 ? 0 : section + 1)
-    // };
-
-    // const previousSlide = () => {
-    //     setSection(section === 0 ? length - 1 : section - 1)
-    // };
-
-    // console.log(section);
-
-    // if(!Array.isArray(slides) || slides.lenght <= 0){
-    //     return null;
-    // };
-
-    var settings = {
+    function SampleNextArrow(props) {
+        const { onClick } = props;
+        return (
+            <img
+                className="right__arrow"
+                src={rightArrow}
+                alt="next slide arrow"
+                onClick={onClick}
+            />
+        );
+      }
+      
+      function SamplePrevArrow(props) {
+        const { onClick } = props;
+        return (
+            <img
+                className="left__arrow"
+                src={leftArrow}
+                alt="prev slide arrow"
+                onClick={onClick}
+            />
+        );
+      }
+    const settings = {
         dots: false,
         infinite: true,
         speed: 500,
         slidesToShow: 4,
         slidesToScroll: 4,
         initialSlide: 0,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />,
         responsive: [
             {
                 breakpoint: 1440,
@@ -58,33 +67,11 @@ const Carousel = ({slides}) => {
       };
 
     return (
-        // <div className="carousel">
-        //     {/* <img src={leftArrow} className="left__arrow" onClick={previousSlide}/>
-        //     <img src={rightArrow} className="right__arrow" onClick={nextSlide}/> */}
-        //     {CarouselData.map((slide, index) => {
-        //         return(
-        //             <div className={index === section ? 'slide active' : 'slide'} key={index}>
-        //                 {index === section && (
-        //                     <div className="slides">
-        //                         <a className="slides__link">
-        //                             <h3 className="slides__link--title">{slide.title}</h3>
-        //                             <img className="slides__link--image"src={slide.image} alt={slide.alt}/>
-        //                             <div className="slides__link--description">
-        //                                 <p>{slide.description}</p>
-        //                             </div>
-        //                         </a>
-        //                     </div>
-        //                 )}
-        //             </div>
-        //         )
-        //     })}
-        // </div>
 
-
-        <Slider {...settings}>
+        <Slider {...settings} className="carousel">
             {CarouselData.map((slide, index) => {
                 return(
-                    <div className="slide" key={index}>
+                    <div className="carousel__slide" key={index}>
                             <div className="slides">
                                 <a className="slides__link">
                                     <h3 className="slides__link--title">{slide.title}</h3>
