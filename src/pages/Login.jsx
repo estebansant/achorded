64bbe0ad997dc22bc4dddf6da35e5e1248f1d354
estebansant <estebansant30@gmail.com>
2022-08-20 09:20:57 -0300

    started to work on the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,31 @@ import '@styles/Login.scss';
 const Login = () => {
   return (
     <main>
-      <h1>Login</h1>
+      <section className="login">
+        <div className="login__container">
+
+          <div className="login__container">
+            <h1 className="login__container--title">Login</h1>
+          </div>
+
+          <form action="/" className="form">
+            <label htmlFor="email" className="form__label">Email address</label>
+            <input type="email" id="email" placeholder="example@address.com" className="form__label--email" />
+            <label htmlFor="password" className="form__label">Enter password</label>
+            <input type="password" id="password" placeholder="********" className="form__label--password" />
+            <input type="submit" value="log in" className="form__login" />
+            <a href="/" className="form__forgot">Forgot my password</a>
+          </form>
+
+          <div classname="signup">
+            <p className="signup__text">Don't have an account?</p>
+            <button className="signup__button">
+              <a href="/ginup" className="signup__button--link">Sign up</a>
+            </button>
+          </div>
+
+        </div>
+      </section>
     </main>
   )
 }
