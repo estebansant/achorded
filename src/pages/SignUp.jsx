6e03989886e604f9d552642a3bdd941bbb3eb522
estebansant <estebansant30@gmail.com>
2022-08-21 18:57:29 -0300

    created and added styles to the sign up page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import '@styles/Signup.scss';
 
 const SignUp = () => {
   return (
     <main>
-      <h1>Sign Up</h1>
+      <section className="signup">
+        <div className="signup__container">
+
+          <div className="signup__title">
+            <h1 className="signup__title--content">Sign-up</h1>
+          </div>
+
+          <form action="/" className="signup__form">
+            <label htmlFor="name" className="signup__form--label">Enter your name</label>
+            <input type="text" id="name" placeholder="Name" className="signup__form--input" />
+            <label htmlFor="email" className="signup__form--label">Enter your email</label>
+            <input type="email" id="email" placeholder="E-mail" className="signup__form--input" />
+            <label htmlFor="password" className="signup__form--label">Create password</label>
+            <input type="password" id="password" placeholder="Password" className="signup__form--input" />
+            <input type="submit" value="Sign up" className="signup__form--sign" />
+          </form>
+
+          <div classname="log">
+            <p className="log__text">Already have an account?</p>
+            <div className="log__link">
+              <a href="/login" className="log__link--content">Log in</a>
+            </div>
+          </div>
+
+        </div>
+      </section>
     </main>
   )
 }
