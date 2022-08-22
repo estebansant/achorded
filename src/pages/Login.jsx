133a05f8dc707c6eea610555f866b9ba83539f41
estebansant <estebansant30@gmail.com>
2022-08-22 20:44:55 -0300

    adding logic to the login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import '@styles/Login.scss';
 
 const Login = () => {
+
+  const loginForm = React.useRef(null)
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(loginForm.current);
+    const data = {
+      username: formData.get('email'),
+      password: formData.get('password'),
+    }
+    console.log(data)
+  }
+
   return (
     <main>
       <section className="login">
@@ -11,16 +24,21 @@ const Login = () => {
             <h1 className="login__title--content">Login</h1>
           </div>
 
-          <form action="/" className="form">
+          <form action="/" className="form" ref={loginForm}>
             <label htmlFor="email" className="form__label">Email address</label>
-            <input type="email" id="email" placeholder="example@address.com" className="form__input" />
+            <input type="email" name="email" placeholder="example@address.com" className="form__input" />
             <label htmlFor="password" className="form__label">Enter password</label>
-            <input type="password" id="password" placeholder="********" className="form__input" />
-            <input type="submit" value="Log in" className="form__login" />
+            <input type="password" name="password" placeholder="********" className="form__input" />
+            <button
+              className="form__login"
+              onClick={handleSubmit}
+            >
+              Log in
+            </button>
             <a href="/" className="form__forgot">Forgot my password</a>
           </form>
 
-          <div classname="create">
+          <div className="create">
             <p className="create__text">Don't have an account?</p>
             <div className="create__link">
               <a href="/signup" className="create__link--content">Sign up</a>
