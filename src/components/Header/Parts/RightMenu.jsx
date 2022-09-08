27d37194210b82cafc9ed8691ffe0d3da84971d2
estebansant <estebansant30@gmail.com>
2022-09-08 20:01:24 -0300

    created styles for the basket sidebar

--- src/components/Header/Parts/RightMenu.jsx
+++ src/components/Header/Parts/RightMenu.jsx
@@ -81,10 +81,10 @@ const RightMenu = ({cart}) => {
                 {toggleBasket &&
                     <motion.div
                         key="cart"
-                        initial={{ opacity: 0, x: "110%" }}
+                        initial={{ opacity: 0, x: "130%" }}
                         animate={{ opacity: 1, x: "0%" }}
                         exit={{ opacity: 0.2,
-                                x: "120%"
+                                x: "130%"
                             }}
                         transition={{ duration: 0.3, ease: "easeInOut" }}
                         className="basket__container"
