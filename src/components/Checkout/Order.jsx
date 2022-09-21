1fa36f771418f5769052f1b5e1f7ae4b2150e86f
estebansant <estebansant30@gmail.com>
2022-09-21 08:16:48 -0300

    fixed checkout products behaviour when deleting them

--- src/components/Checkout/Order.jsx
+++ src/components/Checkout/Order.jsx
@@ -6,28 +6,30 @@ import '@styles/Checkout/Order.scss';
 const Order = ({cart, changeAmount, removeItem}) => {
   return (
     <section className="order">
-        <AnimatePresence>
-            {cart.map(product => (
-                <motion.div
-                    layout
-                    key={`${product.id}-Order`}
-                    initial={{ opacity: 0, x: -250 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    exit={{ opacity: 0.2,
-                            x: -250
-                        }}
-                    transition={{ duration: 0.4, ease: "easeInOut" }}
-                    className="order__content--items"
-                >
-                    <OrderItem
-                        removeItem={removeItem}
-                        changeAmount={changeAmount}
-                        product={product}
-                        key={`orderItem-${product.id}`}
-                    />
-                </motion.div>
-            ))}
-        </AnimatePresence>
+        <div className="order__scroll">
+            <AnimatePresence>
+                {cart.map(product => (
+                    <motion.div
+                        layout
+                        key={`${product.id}-Order`}
+                        initial={{ opacity: 0, x: -250 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0.2,
+                                x: -250
+                            }}
+                        transition={{ duration: 0.4, ease: "easeInOut" }}
+                        className="order__content--items"
+                    >
+                        <OrderItem
+                            removeItem={removeItem}
+                            changeAmount={changeAmount}
+                            product={product}
+                            key={`orderItem-${product.id}`}
+                        />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
+        </div>
     </section>
   )
 }
