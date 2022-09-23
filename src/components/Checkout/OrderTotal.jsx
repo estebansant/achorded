0b66230679c5cda9ee575b8556f4a3929abecd21
estebansant <estebansant30@gmail.com>
2022-09-23 15:35:18 -0300

    created message when trying to make a payment with no items in cart

--- src/components/Checkout/OrderTotal.jsx
+++ src/components/Checkout/OrderTotal.jsx
@@ -8,14 +8,26 @@ import x from '@icons/x_icon.svg';
 const OrderTotal = ({price, cart}) => {
 
   const [ordered, setOrdered] = React.useState(false);
+  const [emptyCheckout, setEmptyCheckout] = React.useState(false);
 
   const handleSubmit = (event) => {
+
     event.preventDefault();
+
+    if(cart.length === 0){   
+      setEmptyCheckout(true);
+    } else {
+      setOrdered(!ordered);
+    }
+  }
+
+  const onToggleOrdered = () => {
     setOrdered(!ordered);
   }
-  const onToggleOrdered = (event) => {
-    setOrdered(!ordered);
+  const onToggleEmptyCheckout = () => {
+    setEmptyCheckout(!emptyCheckout);
   }
+
   const handleCheckout = (cart) => {
     
   }
@@ -106,6 +118,40 @@ const OrderTotal = ({price, cart}) => {
               </div>
                 <h3 className="modal__title">Thanks for your purchase!</h3>
                 <p className="modal__message">You can see the orders you have placed in the "<a href="/orders" className="modal__message--link">my orders</a>" page. Click the link to see what you have bought.
+                </p>
+            </motion.div>
+          </motion.div>
+          }
+        </AnimatePresence>
+
+        <AnimatePresence>
+          {emptyCheckout &&
+          <motion.div 
+            key="empty__background"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0.2,
+              transition: {duration: 0.3}
+            }}
+            transition={{ duration: 0.4, ease: "easeInOut" }}
+            className="modal__background"
+          >
+            <motion.div 
+              key="empty__modal"
+              initial={{ opacity: 0, scale: "0%", x: "-50%", y: "-50%" }}
+              animate={{ opacity: 1, scale: "100%", x: "-50%", y: "-50%" }}
+              exit={{ opacity: 0.2,
+                      scale: "0%", x: "-50%", y: "-50%",
+                      transition: {duration: 0.3}
+                  }}
+              transition={{ duration: 0.4, ease: "easeInOut" }}
+              className="modal"
+            >
+              <div className="modal__x">
+                <img src={x} alt="close" className="modal__x--image" onClick={onToggleEmptyCheckout}/>
+              </div>
+                <h3 className="modal__title">You have no items in cart</h3>
+                <p className="modal__message">Sorry, but you haven't added any item to your cart, if you want to buy a product, please add it to your cart, proceed to the checkout page and finally make the payment.
                 </p>
             </motion.div>
           </motion.div>
