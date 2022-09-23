c668775acc36da9deaeef301cebec8d3cc050171
estebansant <estebansant30@gmail.com>
2022-09-23 15:19:36 -0300

    added animations to the message when empty checkout

--- src/components/Checkout/Order.jsx
+++ src/components/Checkout/Order.jsx
@@ -6,14 +6,26 @@ import '@styles/Checkout/Order.scss';
 const Order = ({cart, changeAmount, removeItem}) => {
   return (
     <section className="order">
-        {cart.length === 0 &&
-            <div className="order__empty">
-                <p className="order__empty--message">You haven't added any item to your cart. 
-                    <br /><br />
-                    <a href="/electric-guitars" className="order__empty--message__link">Browse our shop</a> and pick whatever you like. Those items will appear here once you have added them to the cart.
-                </p>
-            </div>
-        }
+        <AnimatePresence>
+            {cart.length === 0 &&
+                <motion.div 
+                    layout
+                    key="message"
+                    initial={{ opacity: 0, scale: 0, y: "-100%" }}
+                    animate={{ opacity: 1, scale: 1, y: "0%" }}
+                    exit={{ opacity: 0.2,
+                            y: "-120%"
+                        }}
+                    transition={{ duration: 0.4, ease: "easeInOut" }}
+                    className="order__empty"
+                >
+                    <p className="order__empty--message">You haven't added any item to your cart. 
+                        <br /><br />
+                        <a href="/electric-guitars" className="order__empty--message__link">Browse our shop</a> and pick whatever you like. Those items will appear here once you have added them to the cart.
+                    </p>
+                </motion.div>
+            }
+        </AnimatePresence>
         <div className="order__scroll">
             <AnimatePresence>
                 {cart.map(product => (
