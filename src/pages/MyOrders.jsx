930fe0e66ae7732d10f55c9ba3f3ff896e3f30e9
estebansant <estebansant30@gmail.com>
2022-10-22 17:02:22 -0300

    display products of each order

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -1,38 +1,40 @@
 import React from 'react';
+import { motion, AnimatePresence } from "framer-motion";
 import '@styles/Orders/MyOrders.scss';
 
 const MyOrders = ({orders, cart}) => {
 
-  const[display, setDisplay] = React.useState(false);
-
-  const handleItems = (index) =>{
-    setDisplay(!display);
-  }
-
   return (
     <main>
-      <h1>My Orders</h1>
-      {orders.map((acquired, index) => {
+      <h1 className="main__title">My Orders</h1>
+      {orders.map((bought, index) => {
           return(
-            <div key={index}>
-              <h2>Order #{index + 1}</h2>
-              <div>
-                <p>Placed on {acquired.day} at {acquired.hour}</p>
-                <p onClick={() => handleItems(index)}>Show items ({acquired.items.length})</p>
+            <div className="mymyorder" key={index}>
+              <h2 className="myorder__title">Order #{index + 1}</h2>
+              <div className="myorder__left">
+                <p className="myorder__left--date">Placed on {bought.day} at {bought.hour}</p>
+                <div className="myorder__left--items">
+                  <input type="checkbox" className="myorder__left--items__checkbox"/>
+                  <p className="myorder__left--items__text">Show items ({bought.items.length})</p>
+                  <AnimatePresence>
+                    {bought.items &&
+                      <motion.div className="myorder__products">
+                        {bought.items.map((item, i) => {
+                          return(
+                            <div key={i}>
+                              {item.title}
+                            </div>
+                          )
+                        })}
+                      </motion.div>
+                    }
+                  </AnimatePresence>
+                </div>
               </div>
-              <div>
-                <p>Cost: <br /> {acquired.amount}$ USD</p>
-                <p>Payed with: {acquired.payment}</p>
+              <div className="myorder__right">
+                <p className="myorder__right--cost">Cost: <br /> {bought.amount}$ USD</p>
+                <p className="myorder__right--payment">Payed with: {bought.payment}</p>
               </div>
-              {display &&
-                orders.map((data) => data.items.map((bought, i) => {
-                  return(
-                    <div key={i}>
-                      {bought.title}
-                    </div>
-                  )
-                }))
-              }
             </div>
           )
         })
